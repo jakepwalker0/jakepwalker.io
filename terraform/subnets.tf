983baeb7c192704aeea738cc81be6c02b27728af
jakepwalker0 <jakepwalker0@gmail.com>
2020-02-20 15:29:03 +0000

    Add K8s stuff

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -8,3 +8,14 @@ resource "aws_subnet" "eu-west-2-public" {
         Name = "Public Subnet"
     }
 }
+
+resource "aws_subnet" "eu-west-2c-public" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    cidr_block = "${var.public_cidr}"
+    availability_zone = "eu-west-2c"
+
+    tags = {
+        Name = "Public Subnet"
+    }
+}
